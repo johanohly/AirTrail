68b1f7604f21a305a4f89f6f947a64016813ef11
Johan Ohly <johanohly@gmail.com>
2024-09-02 23:43:38 +0200

    chore: explicitly use adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-/** @type {import("@sveltejs/kit").Config}*/
+
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
   preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
   kit: {
