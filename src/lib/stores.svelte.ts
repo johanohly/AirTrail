0a186fd39e56a5bbb5701520af1f8367f5ec412b
ubelhj <37820069+ubelhj@users.noreply.github.com>
2024-11-09 22:08:11 -0800

    make store for map's flight data

--- src/lib/stores.svelte.ts
+++ src/lib/stores.svelte.ts
@@ -1,6 +1,10 @@
+import type { FlightData } from "./utils";
+
 export const openModalsState = $state({
   addFlight: false,
   listFlights: false,
   statistics: false,
   settings: false,
 });
+
+export const filteredMapFlightsState = $state({flightData: [] as FlightData[]});
