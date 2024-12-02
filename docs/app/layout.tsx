f5d09ea01bf4b037b94f223299a2964d2a447e24
Johan Ohly <johanohly@gmail.com>
2024-12-02 18:41:35 +0100

    fix: add metadata to docs

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
 import { Provider } from '@/app/provider';
+import { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -21,3 +22,33 @@ export default function Layout({ children }: Readonly<Props>) {
     </html>
   );
 }
+
+function createMetadata(override: Metadata): Metadata {
+  return {
+    ...override,
+    openGraph: {
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      url: 'https://airtrail.johan.ohly.dk',
+      images: '/dark.png',
+      siteName: 'AirTrail',
+      ...override.openGraph,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: override.title ?? undefined,
+      description: override.description ?? undefined,
+      images: '/dark.png',
+      ...override.twitter,
+    },
+  };
+}
+
+export const metadata = createMetadata({
+  title: {
+    template: '%s | AirTrail',
+    default: 'AirTrail',
+  },
+  description: 'A modern, open-source personal flight tracking system.',
+  metadataBase: new URL('https://airtrail.johan.ohly.dk'),
+});
