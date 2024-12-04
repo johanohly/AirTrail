dc0ea7d3220e38ba10c211b2f6dcac9ff7ef14bf
Johan Ohly <johanohly@gmail.com>
2024-12-04 23:03:30 +0100

    feat: grey-out airports and routes not connected to the currently hovered airport / route

--- src/lib/components/map/index.ts
+++ src/lib/components/map/index.ts
@@ -1,3 +1,5 @@
 export { default as Map } from './Map.svelte';
 export { default as AirportLayer } from './AirportLayer.svelte';
 export { default as FlightArcLayer } from './FlightArcLayer.svelte';
+
+export const INACTIVE_COLOR = [113, 113, 122];
