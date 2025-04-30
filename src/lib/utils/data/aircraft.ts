7dee0822a0f5a57e95b9fbd05f863f6db0e31698
Johan Ohly <johanohly@gmail.com>
2025-04-30 10:02:00 +0200

    fix: stop assuming every aircraft name has a "manufacturer" and "model" and just show the full name

--- src/lib/utils/data/aircraft.ts
+++ src/lib/utils/data/aircraft.ts
@@ -1,8 +1,4 @@
-import { toTitleCase } from '../string';
-
 import { AIRCRAFT } from '$lib/data/aircraft';
-
-const labelCache = new Map<string, string | null>();
 
 export type Aircraft = (typeof AIRCRAFT)[number];
 
@@ -15,20 +11,4 @@ export const WTC_TO_LABEL = {
 
 export const aircraftFromICAO = (icao: string): Aircraft | null => {
   return AIRCRAFT.find((aircraft) => aircraft.icao === icao) ?? null;
-};
-
-export const getAircraftLabel = (icao: string): string | null => {
-  if (labelCache.has(icao)) return labelCache.get(icao)!;
-
-  const aircraft = aircraftFromICAO(icao);
-  if (!aircraft || !aircraft.name) {
-    labelCache.set(icao, null);
-    return null;
-  }
-  const parts = aircraft.name.split(' ');
-  const manufacturer = parts[0]!;
-  const model = parts[1]!;
-  const label = `${toTitleCase(manufacturer)} ${toTitleCase(model)}`;
-  labelCache.set(icao, label);
-  return label;
 };
