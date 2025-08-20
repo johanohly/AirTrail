e8b92814c660466c22c27736c39929915ad80042
Gauthier Canque <10694444+Gaudv@users.noreply.github.com>
2025-08-20 16:03:38 +0200

    feat: add a PieChart for most flown routes (#280)
    Co-authored-by: Gaudv <gitlab.g@canque.fr>
    Co-authored-by: Johan Ohly <johanohly@gmail.com>

--- src/lib/components/modals/statistics/charts/PieCharts.svelte
+++ src/lib/components/modals/statistics/charts/PieCharts.svelte
@@ -84,6 +84,30 @@
     return countByContinent(flights, continents);
   });
 
+  const topRouteDistribution = $derived.by(() => {
+    const counts = flights.reduce<Record<string, number>>((acc, flight) => {
+      const label =
+        (flight.from.iata || flight.from.code) +
+        '-' +
+        (flight.to.iata || flight.to.code);
+      if (label) {
+        acc[label] = (acc[label] || 0) + 1;
+      }
+
+      return acc;
+    }, {});
+
+    return Object.keys(counts).length
+      ? Object.fromEntries(
+          Object.entries(counts)
+            .sort(([, countA], [, countB]) => countB - countA)
+            .slice(0, 5),
+        )
+      : {
+          'No flights with route set': 0,
+        };
+  });
+
   const topAirlineDistribution = $derived.by(() => {
     const counts = flights.reduce<Record<string, number>>((acc, flight) => {
       if (!flight.airline) return acc;
@@ -162,4 +186,5 @@
   <PieChart data={topAirlineDistribution} title="Top Airlines" />
   <PieChart data={topAircraftDistribution} title="Top Aircraft Models" />
   <PieChart data={topAircraftRegDistribution} title="Top Specific Aircrafts" />
+  <PieChart data={topRouteDistribution} title="Top Routes" />
 </div>
