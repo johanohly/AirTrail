1698f0e7422c649a15def7abdffcd2926b623c50
Johan Ohly <johanohly@gmail.com>
2025-08-23 18:54:27 +0200

    fix: correctly format aircraft details using ICAO or name properties

--- src/lib/utils/data/aircraft.ts
+++ src/lib/utils/data/aircraft.ts
@@ -1,7 +1,8 @@
 import type { FlightData } from '$lib/utils';
 
 export const formatAircraft = (flight: FlightData) => {
-  return flight.aircraft && flight.aircraftReg
-    ? `${flight.aircraft} (${flight.aircraftReg})`
-    : flight.aircraft || flight.aircraftReg || 'Unknown';
+  const aircraft = flight.aircraft?.icao || flight.aircraft?.name;
+  return aircraft && flight.aircraftReg
+    ? `${aircraft} (${flight.aircraftReg})`
+    : aircraft || flight.aircraftReg || 'Unknown';
 };
