f1a231697612a03fe1786a0c67d0075d7db49c85
Johan Ohly <johanohly@gmail.com>
2025-08-31 12:43:25 +0200

    docs: update dependencies

--- docs/components/Search.tsx
+++ docs/components/Search.tsx
@@ -1,8 +1,18 @@
 'use client';
 
-import algo from 'algoliasearch/lite';
-import type { SharedProps } from 'fumadocs-ui/components/dialog/search';
-import SearchDialog from 'fumadocs-ui/components/dialog/search-algolia';
+import { liteClient } from 'algoliasearch/lite';
+import {
+  SearchDialog,
+  SearchDialogClose,
+  SearchDialogContent,
+  SearchDialogHeader,
+  SearchDialogIcon,
+  SearchDialogInput,
+  SearchDialogList,
+  SearchDialogOverlay,
+  type SharedProps,
+} from 'fumadocs-ui/components/dialog/search';
+import { useDocsSearch } from 'fumadocs-core/search/client';
 
 const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
 const apiKey = process.env.NEXT_PUBLIC_ALGOLIA_API_KEY;
@@ -10,12 +20,31 @@ const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX;
 
 if (!appId || !apiKey || !indexName) throw new Error('Algolia credentials');
 
-const client = algo(appId, apiKey);
+const client = liteClient(appId, apiKey);
 
-const index = client.initIndex(indexName);
+export default function CustomSearchDialog(props: SharedProps) {
+  const { search, setSearch, query } = useDocsSearch({
+    type: 'algolia',
+    client,
+    indexName: indexName!,
+  });
 
-export default function CustomSearchDialog(
-  props: SharedProps,
-): React.ReactElement {
-  return <SearchDialog index={index} {...props} showAlgolia />;
+  return (
+    <SearchDialog
+      search={search}
+      onSearchChange={setSearch}
+      isLoading={query.isLoading}
+      {...props}
+    >
+      <SearchDialogOverlay />
+      <SearchDialogContent>
+        <SearchDialogHeader>
+          <SearchDialogIcon />
+          <SearchDialogInput />
+          <SearchDialogClose />
+        </SearchDialogHeader>
+        <SearchDialogList items={query.data !== 'empty' ? query.data : null} />
+      </SearchDialogContent>
+    </SearchDialog>
+  );
 }
--- docs/scripts/post-build.mts
+++ docs/scripts/post-build.mts
@@ -1,5 +1,5 @@
 import env from '@next/env';
-import algosearch from 'algoliasearch';
+import { algoliasearch } from 'algoliasearch';
 import { sync } from 'fumadocs-core/search/algolia';
 import * as fs from 'node:fs';
 
@@ -16,7 +16,7 @@ async function main(): Promise<void> {
     throw new Error('NEXT_PUBLIC_ALGOLIA_APP_ID, ALGOLIA_API_KEY are required');
   }
 
-  const client = algosearch(appId, apiKey);
+  const client = algoliasearch(appId, apiKey);
 
   await sync(client, {
     document: process.env.NEXT_PUBLIC_ALGOLIA_INDEX ?? 'document',
