558c61184b32230c9ce7605dd9d7ba0628ffc6ec
Johan Ohly <johanohly@gmail.com>
2025-10-18 18:19:14 +0200

    test: consolidate unit & e2e workflows, allow running actions in forks, and revisit Playwright settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,15 +6,20 @@ dotenv.config();
 export default defineConfig({
   testDir: './tests/e2e',
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 1,
-  reporter: process.env.CI ? 'github' : 'html',
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? '100%' : undefined,
+  reporter: process.env.CI ? [['list'], ['github'], ['html']] : 'list',
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
     video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    locale: 'en-US',
+    timezoneId: 'UTC',
   },
 
-  timeout: 30_000,
+  expect: { timeout: 30000, toPass: { timeout: 30000 } },
+  timeout: process.env.CI ? 30000 : 120000,
 
   projects: [
     {
@@ -23,26 +28,11 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
       fullyParallel: false,
     },
-
     {
       name: 'chromium',
       testIgnore: /setup\/.+\.spec\.ts/,
       use: { ...devices['Desktop Chrome'] },
       dependencies: ['setup'],
     },
-
-    // {
-    //   name: 'firefox',
-    //   testIgnore: /setup\/.+\.spec\.ts/,
-    //   use: { ...devices['Desktop Firefox'] },
-    //   dependencies: ['setup'],
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   testIgnore: /setup\/.+\.spec\.ts/,
-    //   use: { ...devices['Desktop Safari'] },
-    //   dependencies: ['setup'],
-    // },
   ],
 });
