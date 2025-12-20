91d8325ba53c402f1984fd668897fac8b41b1ac3
Johan Ohly <johanohly@gmail.com>
2025-12-20 23:30:51 +0100

    New `MobileFlightList`

--- src/lib/components/modals/list-flights/ListFlightsModal.svelte
+++ src/lib/components/modals/list-flights/ListFlightsModal.svelte
@@ -10,6 +10,7 @@
   import { AirplanemodeInactive } from '@o7/icon/material';
   import { isBefore } from 'date-fns';
 
+  import MobileFlightList from './MobileFlightList.svelte';
   import Toolbar from './Toolbar.svelte';
 
   import AirlineIcon from '$lib/components/display/AirlineIcon.svelte';
@@ -35,6 +36,7 @@
     formatAsMonth,
     isUsingAmPm,
   } from '$lib/utils/datetime';
+  import { isMediumScreen } from '$lib/utils/size';
 
   let {
     open = $bindable<boolean>(),
@@ -216,121 +218,127 @@
     <div class="h-full flex items-center justify-center">
       <AirplanemodeInactive class="text-muted-foreground size-[20dvw]" />
     </div>
+  {:else if !$isMediumScreen}
+    <MobileFlightList {flightsByYear} {selecting} bind:selectedFlights />
   {:else}
     <ScrollArea type="hover">
-      {#each flightsByYear as { year, flights } (year)}
-        {#if year !== '0'}
-          <LabelledSeparator class="mt-4 mb-2">
-            <h3
-              class="border px-4 py-1 rounded-full border-dashed text-sm font-medium leading-7"
-            >
-              {year}
-            </h3>
-          </LabelledSeparator>
-        {/if}
-        <div class="space-y-2" use:autoAnimate>
-          {#each flights as flight (flight.id)}
-            <Card
-              onclick={() => {
-                if (selecting) {
-                  if (selectedFlights.includes(flight.id)) {
-                    selectedFlights = selectedFlights.filter(
-                      (id) => id !== flight.id,
-                    );
-                  } else {
-                    selectedFlights = [...selectedFlights, flight.id];
-                  }
-                }
-              }}
-              level="2"
-              class={cn('flex items-center p-3', {
-                'cursor-pointer border-zinc-600 border-dotted border-2':
-                  selecting,
-                'border-destructive border-solid':
-                  selecting && selectedFlights.includes(flight.id),
-              })}
-            >
-              <div
-                class="flex items-stretch md:items-center max-md:flex-col-reverse max-md:content-start flex-1 h-full min-w-0"
+      <div class="hidden md:block">
+        {#each flightsByYear as { year, flights } (year)}
+          {#if year !== '0'}
+            <LabelledSeparator class="mt-4 mb-2">
+              <h3
+                class="border px-4 py-1 rounded-full border-dashed text-sm font-medium leading-7"
               >
-                {#if flight.month}
-                  <div class="max-md:hidden flex justify-center shrink-0 w-11">
-                    <AirlineIcon
-                      airline={flight.airline}
-                      size={36}
-                      fallback="plane"
+                {year}
+              </h3>
+            </LabelledSeparator>
+          {/if}
+          <div class="space-y-2" use:autoAnimate>
+            {#each flights as flight (flight.id)}
+              <Card
+                onclick={() => {
+                  if (selecting) {
+                    if (selectedFlights.includes(flight.id)) {
+                      selectedFlights = selectedFlights.filter(
+                        (id) => id !== flight.id,
+                      );
+                    } else {
+                      selectedFlights = [...selectedFlights, flight.id];
+                    }
+                  }
+                }}
+                level="2"
+                class={cn('flex items-center p-3', {
+                  'cursor-pointer border-zinc-600 border-dotted border-2':
+                    selecting,
+                  'border-destructive border-solid':
+                    selecting && selectedFlights.includes(flight.id),
+                })}
+              >
+                <div
+                  class="flex items-stretch md:items-center max-md:flex-col-reverse max-md:content-start flex-1 h-full min-w-0"
+                >
+                  {#if flight.month}
+                    <div
+                      class="max-md:hidden flex justify-center shrink-0 w-11"
+                    >
+                      <AirlineIcon
+                        airline={flight.airline}
+                        size={36}
+                        fallback="plane"
+                      />
+                    </div>
+                    <Separator
+                      orientation="vertical"
+                      class="max-md:hidden h-10 mx-3"
                     />
-                  </div>
-                  <Separator
-                    orientation="vertical"
-                    class="max-md:hidden h-10 mx-3"
-                  />
-                  <div
-                    class={cn(
-                      'max-md:hidden flex flex-col shrink-0',
-                      isUsingAmPm() ? 'w-36' : 'w-32',
-                    )}
-                  >
-                    {@render flightTimes(flight)}
-                  </div>
-                  <div class="px-4 flex md:hidden">
                     <div
                       class={cn(
-                        'flex flex-col shrink-0',
+                        'max-md:hidden flex flex-col shrink-0',
                         isUsingAmPm() ? 'w-36' : 'w-32',
                       )}
                     >
                       {@render flightTimes(flight)}
                     </div>
-                    <div class="hidden sm:flex flex-col">
-                      {@render seatAndAirline(flight)}
-                    </div>
-                    {#if !readonly}
-                      <div class="flex justify-end w-full">
-                        {@render actions(flight)}
+                    <div class="px-4 flex md:hidden">
+                      <div
+                        class={cn(
+                          'flex flex-col shrink-0',
+                          isUsingAmPm() ? 'w-36' : 'w-32',
+                        )}
+                      >
+                        {@render flightTimes(flight)}
                       </div>
-                    {/if}
+                      <div class="hidden sm:flex flex-col">
+                        {@render seatAndAirline(flight)}
+                      </div>
+                      {#if !readonly}
+                        <div class="flex justify-end w-full">
+                          {@render actions(flight)}
+                        </div>
+                      {/if}
+                    </div>
+                    <Separator class="my-4 md:hidden" />
+                  {/if}
+                  <div class="max-lg:hidden flex flex-col w-48 shrink-0">
+                    {@render seatAndAirline(flight)}
                   </div>
-                  <Separator class="my-4 md:hidden" />
-                {/if}
-                <div class="max-lg:hidden flex flex-col w-48 shrink-0">
-                  {@render seatAndAirline(flight)}
-                </div>
-                <div class="max-xl:hidden flex flex-col w-48 shrink-0">
-                  {@render flightAndTailNumber(flight)}
-                </div>
-                <div class="flex flex-1 px-12 md:px-16">
-                  <div class="w-full grid grid-cols-[auto_1fr_auto] gap-3">
-                    {@render airport(flight.from)}
-                    <div class="h-full flex flex-col justify-center">
-                      <div class="relative">
-                        <div
-                          class="relative w-full h-px border-b border-dashed border-dark-2 dark:border-zinc-500"
-                        >
+                  <div class="max-xl:hidden flex flex-col w-48 shrink-0">
+                    {@render flightAndTailNumber(flight)}
+                  </div>
+                  <div class="flex flex-1 px-12 md:px-16">
+                    <div class="w-full grid grid-cols-[auto_1fr_auto] gap-3">
+                      {@render airport(flight.from)}
+                      <div class="h-full flex flex-col justify-center">
+                        <div class="relative">
                           <div
-                            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-1 bg-card dark:bg-dark-2 text-dark-2 dark:text-zinc-500"
+                            class="relative w-full h-px border-b border-dashed border-dark-2 dark:border-zinc-500"
                           >
-                            <div class="flex flex-col items-center">
-                              <Plane size="20" />
-                              <span class="text-xs">{flight.duration}</span>
+                            <div
+                              class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-1 bg-card dark:bg-dark-2 text-dark-2 dark:text-zinc-500"
+                            >
+                              <div class="flex flex-col items-center">
+                                <Plane size="20" />
+                                <span class="text-xs">{flight.duration}</span>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
+                      {@render airport(flight.to)}
                     </div>
-                    {@render airport(flight.to)}
                   </div>
+                  {#if !readonly}
+                    <div class="hidden md:flex">
+                      {@render actions(flight)}
+                    </div>
+                  {/if}
                 </div>
-                {#if !readonly}
-                  <div class="hidden md:flex">
-                    {@render actions(flight)}
-                  </div>
-                {/if}
-              </div>
-            </Card>
-          {/each}
-        </div>
-      {/each}
+              </Card>
+            {/each}
+          </div>
+        {/each}
+      </div>
     </ScrollArea>
   {/if}
 </Modal>
