4d9868150def87032970e7afca55efff903dd8be
Johan Ohly <johanohly@gmail.com>
2026-02-23 23:07:10 +0100

    Small adjustments

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const origin = process.env.ORIGINS || process.env.ORIGIN;
 
